b2ee73cd51b914194123414db6c8742b8c7e6f39
2027summer <283045080+2027summer@users.noreply.github.com>
2026-05-10 04:17:15 +0000

    feat: add more syscall struct

--- src/syscall_event.h
+++ src/syscall_event.h
@@ -26,11 +26,88 @@ struct ExecveData {
     std::vector<std::string> envp;
 };
 
+struct ChdirData {
+    std::string filename;
+};
+
+struct UnlinkAtData {
+    int dfd;
+    std::string pathname;
+    int flags;
+};
+
+struct RenameData {
+    std::string oldname;
+    std::string newname;
+};
+
+struct RenameAtData {
+    int oldfd;
+    std::string oldname;
+    int newfd;
+    std::string newname;
+};
+
+struct RenameAt2Data {
+    int oldfd;
+    std::string oldname;
+    int newfd;
+    std::string newname;
+    unsigned int flags;
+};
+
+struct LinkAtData {
+    int oldfd;
+    std::string oldname;
+    int newfd;
+    std::string newname;
+    int flags;
+};
+
+struct SymlinkAtData {
+    std::string oldname;
+    int newdfd;
+    std::string newname;
+};
+
+struct ReadlinkAtData {
+    int dfd;
+    std::string path;
+    std::vector<char> buf;
+    int bufsiz;
+};
+
+struct Dup2Data {
+    unsigned int oldfd;
+    unsigned int newfd;
+};
+
+struct CloseData {
+    unsigned int fd;
+};
+
+struct ReadData {
+    unsigned int fd;
+    std::vector<char> buf;
+    size_t count;
+};
+
 using SyscallArgs = std::variant<
     std::monostate,
     OpenAtData,
     WriteData,
-    ExecveData
+    ExecveData,
+    ChdirData,
+    UnlinkAtData,
+    RenameData,
+    RenameAtData,
+    RenameAt2Data,
+    LinkAtData,
+    SymlinkAtData,
+    ReadlinkAtData,
+    Dup2Data,
+    CloseData,
+    ReadData
 >;
 
 struct SyscallEvent {
