7dd71ddc2ddcffa52fca5b30ea617c97e9291d1b
2027summer <283045080+2027summer@users.noreply.github.com>
2026-05-10 04:19:18 +0000

    feat: implement parse_* functions

--- src/parse_syscall.cc
+++ src/parse_syscall.cc
@@ -120,4 +120,166 @@ namespace engine {
             .mode = mode
         };
     }
+
+    std::optional<ChdirData> parse_chdir(pid_t pid, __ptrace_syscall_info info) {
+        auto filename = read_child_string(pid, info.entry.args[0]);
+
+        if (!filename.has_value()) {
+            return std::nullopt;
+        }
+
+        return ChdirData {
+            .filename = *filename
+        };
+    }
+
+    std::optional<UnlinkAtData> parse_unlinkat(pid_t pid, __ptrace_syscall_info info) {
+        int dfd = static_cast<int>(info.entry.args[0]);
+        auto pathname = read_child_string(pid, info.entry.args[1]);
+        int flags = static_cast<int>(info.entry.args[2]);
+
+        if (!pathname.has_value()) {
+            return std::nullopt;
+        }
+
+        return UnlinkAtData {
+            .dfd = dfd,
+            .pathname = *pathname,
+            .flags = flags
+        };
+    }
+
+    std::optional<RenameData> parse_rename(pid_t pid, __ptrace_syscall_info info) {
+        auto oldname = read_child_string(pid, info.entry.args[0]);
+        auto newname = read_child_string(pid, info.entry.args[1]);
+
+        if (!oldname.has_value() || !newname.has_value()) {
+            return std::nullopt;
+        }
+
+        return RenameData {
+            .oldname = *oldname,
+            .newname = *newname
+        };
+    }
+
+    std::optional<RenameAtData> parse_renameat(pid_t pid, __ptrace_syscall_info info) {
+        int oldfd = static_cast<int>(info.entry.args[0]);
+        auto oldname = read_child_string(pid, info.entry.args[1]);
+        int newfd = static_cast<int>(info.entry.args[2]);
+        auto newname = read_child_string(pid, info.entry.args[3]);
+
+        if (!oldname.has_value() || !newname.has_value()) {
+            return std::nullopt;
+        }
+
+        return RenameAtData {
+            .oldfd = oldfd,
+            .oldname = *oldname,
+            .newfd = newfd,
+            .newname = *newname
+        };
+    }
+
+    std::optional<RenameAt2Data> parse_renameat2(pid_t pid, __ptrace_syscall_info info) {
+        int oldfd = static_cast<int>(info.entry.args[0]);
+        auto oldname = read_child_string(pid, info.entry.args[1]);
+        int newfd = static_cast<int>(info.entry.args[2]);
+        auto newname = read_child_string(pid, info.entry.args[3]);
+        unsigned int flags = static_cast<unsigned int>(info.entry.args[4]);
+
+        if (!oldname.has_value() || !newname.has_value()) {
+            return std::nullopt;
+        }
+
+        return RenameAt2Data {
+            .oldfd = oldfd,
+            .oldname = *oldname,
+            .newfd = newfd,
+            .newname = *newname,
+            .flags = flags
+        };
+    }
+
+    std::optional<LinkAtData> parse_linkat(pid_t pid, __ptrace_syscall_info info) {
+        int oldfd = static_cast<int>(info.entry.args[0]);
+        auto oldname = read_child_string(pid, info.entry.args[1]);
+        int newfd = static_cast<int>(info.entry.args[2]);
+        auto newname = read_child_string(pid, info.entry.args[3]);
+        int flags = static_cast<int>(info.entry.args[4]);
+
+        if (!oldname.has_value() || !newname.has_value()) {
+            return std::nullopt;
+        }
+
+        return LinkAtData {
+            .oldfd = oldfd,
+            .oldname = *oldname,
+            .newfd = newfd,
+            .newname = *newname,
+            .flags = flags
+        };
+    }
+
+    std::optional<SymlinkAtData> parse_symlinkat(pid_t pid, __ptrace_syscall_info info) {
+        auto oldname = read_child_string(pid, info.entry.args[0]);
+        int newdfd = static_cast<int>(info.entry.args[1]);
+        auto newname = read_child_string(pid, info.entry.args[2]);
+
+        if (!oldname.has_value() || !newname.has_value()) {
+            return std::nullopt;
+        }
+
+        return SymlinkAtData {
+            .oldname = *oldname,
+            .newdfd = newdfd,
+            .newname = *newname
+        };
+    }
+
+    std::optional<ReadlinkAtData> parse_readlinkat(pid_t pid, __ptrace_syscall_info info) {
+        int dfd = static_cast<int>(info.entry.args[0]);
+        auto path = read_child_string(pid, info.entry.args[1]);
+        int bufsiz = static_cast<int>(info.entry.args[3]);
+
+        if (!path.has_value()) {
+            return std::nullopt;
+        }
+
+        return ReadlinkAtData {
+            .dfd = dfd,
+            .path = *path,
+            .buf = {},
+            .bufsiz = bufsiz
+        };
+    }
+
+    std::optional<WriteData> parse_write(pid_t pid, __ptrace_syscall_info info) {
+        unsigned int fd = static_cast<unsigned int>(info.entry.args[0]);
+        auto data = read_child_bytes(pid, info.entry.args[1], info.entry.args[2]);
+        size_t count = static_cast<size_t>(info.entry.args[2]);
+
+        if (!data.has_value()) {
+            return std::nullopt;
+        }
+
+        return WriteData {
+            .fd = fd,
+            .data = *data,
+            .count = count
+        };
+    }
+
+    std::optional<Dup2Data> parse_dup2(pid_t, __ptrace_syscall_info info) {
+        return Dup2Data {
+            .oldfd = static_cast<unsigned int>(info.entry.args[0]),
+            .newfd = static_cast<unsigned int>(info.entry.args[1])
+        };
+    }
+
+    std::optional<CloseData> parse_close(pid_t, __ptrace_syscall_info info) {
+        return CloseData {
+            .fd = static_cast<unsigned int>(info.entry.args[0])
+        };
+    }
 }
